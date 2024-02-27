d7e8dc78072d8fd910a42a3df959055da6490e19
SimpsonChiwashira <127128430+SimpsonChiwashira@users.noreply.github.com>
2024-02-27 00:40:27 +0000

    Updated Version Build to Support Static Files

--- rosemary/settings.py
+++ rosemary/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-rg^m(fw#o05y75hw8dr7q$zrs)5lg7j9z+xhdt%(9j36b8uwlv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -117,7 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIR = [ BASE_DIR / "static" ]
+#STATICFILES_DIR = [ BASE_DIR / "static" ]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
